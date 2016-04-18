1671f1cd059a14afb7e68c7b80b7919d45904702
tripperroc <cmh@Christophers-MacBook-Pro.local>
2016-04-17 22:30:15 -0400

    hopefully next check-in works

--- compareResultsMCMC.py
+++ compareResultsMCMC.py
@@ -102,7 +102,7 @@ def main():
     global name
     DG = pickle.load(open('/usr/space1/uvb6476/directed_followers_graph.pkl','rb'))
     getWordGraph('/usr/space1/uvb6476/wordGraph_200.pkl')
-    filename = file("CompareResultsNodes.xlsx","wb")
+    filename = file("CompareResultsMCMC.xlsx","wb")
     workbook = xlsxwriter.Workbook(filename)
     bold = workbook.add_format({'bold': True})
     UG = DG.to_undirected()
@@ -117,23 +117,57 @@ def main():
         countAvgClusteringCoeff = 0
         countAvgTriangles = 0
         countLargestCC = 0
+
+        worksheet = workbook.add_worksheet(name)
+        worksheet.set_column(0, 4, 30)
+        worksheet.write(0,1,"Master Graph",bold)
+        worksheet.write(0,2,"Word Graph",bold)
+        worksheet.write(0,3," % Master Subgraphs > wordGraphs",bold)
+        worksheet.write(0,4," % Master Subgraphs > wordGraphs",bold)
+        worksheet.write(1,0,"Number Of Edges",bold)
+        worksheet.write(2,0,"Average Degree",bold)
+        worksheet.write(3,0,"Average Clustering Coefficient",bold)
+        worksheet.write(4,0,"Average number of triangles",bold)
+        worksheet.write(5,0,"Largest Connected Component",bold)
+        
         #worksheet.write(6,0,"KCore",bold)
         #worksheet.write(7,0,"Graph",bold)
         graph =  wordGraph[value]
         undirectedGraph = graph.to_undirected()
-	numberOfNodes =  nx.number_of_nodes(undirectedGraph) 
-        
-	print numberOfNodes
-        print nx.number_of_edges(undirectedGraph)
+        numberOfNodes =  nx.number_of_nodes(undirectedGraph) 
+        #print numberOfNodes
+        #print nx.number_of_edges(undirectedGraph)
         row = 5
         for i in range(0,1000):
-            
-	    #masterGraphNodes = random.sample(nodes,numberOfNodes)
+            worksheet.write(row + 1,0,"Random Subgraph #"+str(i+1),bold)
+            worksheet.write(row + 2,0,"Number Of Edges",bold)
+            worksheet.write(row + 3,0,"Average Degree",bold)
+            worksheet.write(row + 4,0,"Average Clustering Coefficient",bold)
+            worksheet.write(row + 5,0,"Average number of triangles",bold)
+            worksheet.write(row + 6,0,"Largest Connected Component",bold)
+
+	       
             masterGraphNodes = mcmc.mcmc_subgraph_sample(UG, undirectedGraph)
-            masterGraph = UG.subgraph(masterGraphNodes)
-            print nx.number_of_edges(masterGraph)
+            undirectedMasterGraph = UG.subgraph(masterGraphNodes)
+
+            nodesResult = compareNumberOfEdges(undirectedMasterGraph,graph,worksheet,row + 2)
+            if nodesResult == 1:
+                countNodes = countNodes + 1
+            degreeResult = compareAvgDegree(undirectedMasterGraph,graph,worksheet,row + 3)
+            if degreeResult == 1:
+                countDegree = countDegree + 1
+            clusteringCoeffResult = compareAvgClusteringCoeff(undirectedMasterGraph,undirectedGraph,worksheet,row + 4)
+            if clusteringCoeffResult == 1:
+                countAvgClusteringCoeff = countAvgClusteringCoeff + 1
+            numberOfTrianglesResult = compareAvgNumberOfTriangles(undirectedMasterGraph,undirectedGraph,worksheet,row + 5)
+            if numberOfTrianglesResult == 1:
+                countAvgTriangles = countAvgTriangles + 1
+            LargestCCResult = compareLargestCC(undirectedMasterGraph,undirectedGraph,worksheet,row + 6)
+            if LargestCCResult == 1:
+                countLargestCC = countLargestCC + 1
+            row = row + 6
             
-        '''
+        
         worksheet.write(1,1,countNodes,bold)
         worksheet.write(1,2,(1000 - countNodes),bold)
         worksheet.write(1,4,(countNodes/10),bold)
@@ -149,8 +183,8 @@ def main():
         worksheet.write(5,1,countLargestCC,bold)
         worksheet.write(5,2,(1000 - countLargestCC),bold)
         worksheet.write(5,4,(countLargestCC/10),bold)
-        '''
-    #workbook.close()
+        
+    workbook.close()
 
 if __name__ == "__main__":
     main()# -*- coding: utf-8 -*-
